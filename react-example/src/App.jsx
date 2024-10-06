66a1e33e18f2145c6e42ef4287cb2055d46153bc
cinthiaastoraiqueh <cinthiaastoraique2@gmail.com>
2024-10-05 20:59:21 -0400

    Initial commit for LAB-07

---
+++ react-example/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import { WapperList } from './components/WrapperList'; 
+import { List } from './components/List';
+
+
+function App() {
+  
+  return (
+    <>
+      <WapperList>
+        <List/>
+      </WapperList>
+       
+    </>
+  )
+}
+
+export default App
